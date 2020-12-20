ac9808b3d040bcc19d9212c65272ae5130df618c
Taras Senko <taras.senko.99@gmail.com>
2020-12-20 03:43:50 +0200

    update(map): develop section

--- src/index.js
+++ src/index.js
@@ -8,4 +8,27 @@ new Swiper('.swiper-container', {
 	},
 })
 
+let centered
+ if(window.innerWidth < 721){
+	console.log('work2')
+	centered = [55.771064, 37.588629];
+}else if (window.innerWidth < 1200){
+	console.log('work')
+	centered = [55.771064, 37.578684];
+}  else {
+	centered = [55.771064, 37.570684];
+}
 
+let myMap;
+ymaps.ready(init);
+function init () {
+	myMap = new ymaps.Map('map', {
+		center: centered,
+		zoom: 14.5
+	})
+	var myPlacemark = new ymaps.Placemark([55.771064, 37.588629], null,{
+		preset: 'islands#blueMoneyIcon'
+	});
+	myMap.geoObjects.add(myPlacemark);
+	myMap.behaviors.disable('scrollZoom');
+};
