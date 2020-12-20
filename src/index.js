812e1c2c153c8a8112247ee1ac557e61381ac276
mrAzay <furoxaz@gmail.com>
2020-12-20 18:19:32 +0200

    aa

--- src/index.js
+++ src/index.js
@@ -53,19 +53,68 @@ if (window.innerWidth < 721) {
 	centered = [55.771064, 37.570684];
 }
 
-let myMap;
-ymaps.ready(init);
-function init() {
-	myMap = new ymaps.Map('map', {
-		center: centered,
-		zoom: 14.5
-	})
-	var myPlacemark = new ymaps.Placemark([55.771064, 37.588629], null, {
-		iconLayout: 'default#image',
-		iconImageHref: 'img/image6.jpg',
-		iconImageSize: [50, 50],
-		iconImageOffset: [-5, -38]
-	});
-	myMap.geoObjects.add(myPlacemark);
-	myMap.behaviors.disable('scrollZoom');
-};
+
+
+
+ymaps.ready(function () {
+	var myMap = new ymaps.Map('map', {
+					center: [55.751574, 37.573856],
+					zoom: 9
+			}, {
+					searchControlProvider: 'yandex#search'
+			}),
+
+			// Creating a content layout.
+			MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+					'<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+			),
+
+			myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+					hintContent: 'A custom placemark icon',
+					balloonContent: 'This is a pretty placemark'
+			}, {
+					/**
+					 * Options.
+					 * You must specify this type of layout.
+					 */
+					iconLayout: 'default#image',
+					// Custom image for the placemark icon.
+					iconImageHref: 'map.svg',
+					// The size of the placemark.
+					iconImageSize: [30, 42],
+					/**
+					 * The offset of the upper left corner of the icon relative
+					 * to its "tail" (the anchor point).
+					 */
+					iconImageOffset: [-5, -38]
+			}),
+
+			myPlacemarkWithContent = new ymaps.Placemark([55.661574, 37.573856], {
+					hintContent: 'A custom placemark icon with contents',
+					balloonContent: 'This one — for Christmas',
+					iconContent: '12'
+			}, {
+					/**
+					 * Options.
+					 * You must specify this type of layout.
+					 */
+					iconLayout: 'default#imageWithContent',
+					// Custom image for the placemark icon.
+					iconImageHref: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/How_to_use_icon.svg/1200px-How_to_use_icon.svg.png',
+					// The size of the placemark.
+					iconImageSize: [48, 48],
+					/**
+					 * The offset of the upper left corner of the icon relative
+					 * to its "tail" (the anchor point).
+					 */
+					iconImageOffset: [-24, -24],
+					// Offset of the layer with content relative to the layer with the image.
+					iconContentOffset: [15, 15],
+					// Content layout.
+					iconContentLayout: MyIconContentLayout
+			});
+
+	myMap.geoObjects
+			.add(myPlacemark)
+			.add(myPlacemarkWithContent);
+});
