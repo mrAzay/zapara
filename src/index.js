e26d0b223628e2140e35746fd8a536f2802f73a9
Taras Senko <taras.senko.99@gmail.com>
2020-12-22 01:40:46 +0200

    тупое гавно тупого говна

--- src/index.js
+++ src/index.js
@@ -168,3 +168,25 @@ ymaps.ready(function() {
 		}
 	})
 });
+
+// accordion spiszheno
+
+let accordion = document.querySelector('.accordion');
+let items = accordion.querySelectorAll('.accordion__item');
+let title = accordion.querySelectorAll('.accordion__title');
+
+function toggleAccordion() {
+	let thisItem = this.parentNode;
+
+	items.forEach(item => {
+		if (thisItem == item ) {
+			// if this item is equal to the clicked item, open it.
+			thisItem.classList.toggle('active');
+			return;
+		}
+		// otherwise, remove the open class
+		item.classList.remove('active');
+	});
+}
+
+title.forEach(question => question.addEventListener('click', toggleAccordion));
